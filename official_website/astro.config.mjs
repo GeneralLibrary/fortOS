42bfa89b1a8d620407887384262490a075786ea1
JusterZhu <zhuzhen723723@outlook.com>
2026-08-08 16:08:32 +0800

    update

--- official_website/astro.config.mjs
+++ official_website/astro.config.mjs
@@ -6,7 +6,7 @@ import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fortos.dev",
+  site: "https://fortos.justerzhu.cn",
   i18n: {
     defaultLocale: "en",
     locales: ["en", "zh"],
